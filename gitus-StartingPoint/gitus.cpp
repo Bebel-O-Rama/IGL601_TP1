824d460fa98eef32f0b8e5086cca1ff9d52e608c
Bebel-O-Rama <bebel-o-rama@hotmail.com>
2021-05-17 18:20:54 -0700

    Initial project

---
+++ gitus-StartingPoint/gitus.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <dummy.h>
+
+int main(int argc, char * arcv[])
+{
+    std::cout << arcv[1] << std::endl;
+
+	// TestDeTrucs();
+    // std::cout << GetAnswerToLifeUniverseAndEverything() << std::endl;
+    return 0;
+}
+
+
+
